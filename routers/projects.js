d7f12968794d2f7fce1dd8da273ad444a381bcca
thibautbessone <thibaut13b@gmail.com>
2020-08-11 22:35:38 +0200

    Added githug links & updated CSS

--- routers/projects.js
+++ routers/projects.js
@@ -3,10 +3,10 @@ const request = require('request-promise');
 const md = require('marked');
 
 const projectsMap = new Map();
-projectsMap.set('bluebot', 'https://raw.githubusercontent.com/thibautbessone/DiscordBlueBot/master/README.md');
-projectsMap.set('selfbot', 'https://raw.githubusercontent.com/thibautbessone/DiscordSelfBlueBot/master/README.md');
-projectsMap.set('shinrai', 'https://raw.githubusercontent.com/thibautbessone/shinrai/master/README.md');
-projectsMap.set('randomizeit', 'https://raw.githubusercontent.com/thibautbessone/randomizeit/master/README.md');
+projectsMap.set('bluebot', '/thibautbessone/DiscordBlueBot/');
+projectsMap.set('selfbot', '/thibautbessone/DiscordSelfBlueBot/');
+projectsMap.set('shinrai', '/thibautbessone/shinrai/');
+projectsMap.set('randomizeit', '/thibautbessone/randomizeit/');
 
 const projectsRouter = new Router({
     prefix: '/projects'
@@ -17,16 +17,18 @@ projectsRouter.get('/', async (ctx) => {
 });
 
 projectsRouter.get('/:project', async (ctx) => {
-    const url = projectsMap.get(ctx.params.project);
-    if(url === undefined) {
+    const proj = projectsMap.get(ctx.params.project);
+    const repoUrl = 'https://github.com' + proj;
+    const readmeUrl = 'https://raw.githubusercontent.com' + proj + 'master/README.md';
+    if(readmeUrl === undefined) {
         await ctx.render('notFound');
         return;
     }
-    let markdown = await request.get(url, function (err, res) {
+    let markdown = await request.get(readmeUrl, function (err, res) {
         if (err) console.log(err);
         return res.body;
     });
-    await ctx.render('project', { md: md, readme: markdown});
+    await ctx.render('project', { md: md, readme: markdown, repoUrl: repoUrl});
 
 });
 
